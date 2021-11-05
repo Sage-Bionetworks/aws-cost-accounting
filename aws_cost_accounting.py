746a78aefaa26ed580fd348f8751376059a9b571
bhoff <bruce.hoff@sagebase.org>
2021-11-05 10:39:11 -0700

    First step done:  We can bin all CUR costs as INDIRECT or as some label

--- aws_cost_accounting.py
+++ aws_cost_accounting.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import math
 from IPython.display import display
 
 # TODO CUR report will be read from AWS, not from a local file
@@ -7,6 +6,7 @@ CUR_FILE = '2021Sept_CUR.parquet'
 
 ACCOUNTS_FILE = 'aws_accounts.tsv'
 
+# These are the accounts that require special handling
 SCICOMP_ACCOUNT =       '055273631518'
 SANDBOX_ACCOUNT =       '563295687221'
 SCIPOOL_PROD_ACCOUNT =  '237179673806'
@@ -22,6 +22,7 @@ SCICOMP_INDIRECT_TAGS = ['Infrastructure', 'Infra', 'Infrastucture', 'General']
 INDIRECT_LABEL = 'INDIRECT'
 
 def main():
+	# Read in the account labels, for those accounts whose costs are all allocated the same way
 	accounts_columns = {
 		'account_id':'str',
 		'account_name':'str',
@@ -30,6 +31,7 @@ def main():
 	accounts = pd.read_csv(ACCOUNTS_FILE, sep='\t', usecols=accounts_columns.keys(), dtype=accounts_columns)	
 	accounts = accounts[['account_id','account_label']]
 
+	# Read in the month's cost and usage report
 	columns=[
   		VALUE_COLUMN,
   		ACCOUNT_ID_COLUMN,
@@ -37,24 +39,28 @@ def main():
 	]
 	df = pd.read_parquet(CUR_FILE, columns=columns)
 	
-	# Compute total cost
+	# Compute total cost for the month
 	total_cost = df[VALUE_COLUMN].sum()
 	
 	# we now want to inner join df with accounts on ACCOUNT_ID_COLUMN <-> 'account_id'
 	accounts = accounts.rename(columns={'account_id': ACCOUNT_ID_COLUMN})
-	joined = df.set_index(ACCOUNT_ID_COLUMN).join(accounts.set_index(ACCOUNT_ID_COLUMN), on=ACCOUNT_ID_COLUMN, how="inner")
+	joined = df.join(accounts.set_index(ACCOUNT_ID_COLUMN), on=ACCOUNT_ID_COLUMN, how="inner")
 	
 	# group expenses by their category.  See group_by() for details
 	gb = joined.groupby((lambda row_index: group_by(joined,row_index)), dropna=False, group_keys=False)
+	
 	# sum up each category
 	summarized = gb.sum().sort_values(VALUE_COLUMN)
+	
 	# sort, descending
 	sorted = summarized.sort_values(VALUE_COLUMN, ascending=False)[[VALUE_COLUMN]]
+	
 	# display the result
 	sorted = sorted.rename(columns={VALUE_COLUMN:DISPLAY_VALUE_COLUMN})
 	with pd.option_context( 'display.precision', 2),\
 		pd.option_context('display.float_format', (lambda x : f'${x:.2f}')):
 		display(sorted)
+		
 	total = sorted[DISPLAY_VALUE_COLUMN].sum()
 	if (abs(total-total_cost)>0.01):
 		raise Exception("categorized costs to not add up to total bill.")
@@ -68,14 +74,14 @@ def main():
 
 
 def group_by(df, row_index): 
-	if df.at[row_index, 'account_label'][0] != 'na':
-		return df.at[row_index, 'account_label'][0]
+	if df.at[row_index, 'account_label'] != 'na':
+		return df.at[row_index, 'account_label']
 	# if it IS a 'na' then we are in one of the accounts that needs further evaluation
-	if df.at[row_index, PROJECT_COLUMN][0] is None or df.at[row_index, PROJECT_COLUMN][0].isna():
+	if df.at[row_index, PROJECT_COLUMN] is None:
 		return('INDIRECT') # these are costs we can't map, so we just call them indirect
-	if df.at[row_index, PROJECT_COLUMN][0] in SCICOMP_INDIRECT_TAGS:
+	if df.at[row_index, PROJECT_COLUMN] in SCICOMP_INDIRECT_TAGS:
 		return('INDIRECT') # these are tagged with an indirect tag
-	return df.at[row_index, PROJECT_COLUMN][0] # these have a direct tag
+	return df.at[row_index, PROJECT_COLUMN] # these have a direct tag
 
 	
 if __name__ == "__main__":
